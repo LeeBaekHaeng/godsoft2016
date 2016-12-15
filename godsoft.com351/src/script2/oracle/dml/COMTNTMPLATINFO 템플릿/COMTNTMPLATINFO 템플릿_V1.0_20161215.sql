1f61f7011607bbb4a8f595909fc80a25e0478958
이백행 <dlqorgod@naver.com>
2016-12-15 09:21:48 +0900

    게시판

---
+++ godsoft.com351/src/script2/oracle/dml/COMTNTMPLATINFO 템플릿/COMTNTMPLATINFO 템플릿_V1.0_20161215.sql
@@ -0,0 +1,14 @@
+select
+    *
+from COMTNTMPLATINFO /* ≈€«√∏¥ */
+;
+
+select
+    *
+from COMTNTMPLATINFO COM_TN_TMPLAT_INFO /* ≈€«√∏¥ */
+;
+
+select
+    *
+from COMTNTMPLATINFO TMPLAT_INFO /* ≈€«√∏¥ */
+;
